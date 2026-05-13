c6edcec44572950e5f980971da49ff81d7a3ef63
j <rafadevvv@gmail.com>
2026-05-13 10:19:50 -0300

    Adição do item do mato para aparecer futemons e placeholder da batalha

--- person/player.gd
+++ person/player.gd
@@ -4,6 +4,33 @@ const VELOCIDADE = 200
 
 var situationAtual = "idle"
 
+var matos = []
+
+func _ready():
+	randomize()
+
+func entrou_no_mato(mato):
+	matos.append(mato)
+
+func saiu_do_mato(mato):
+	matos.erase(mato)
+
+func tentar_encontro():
+
+	if matos.is_empty():
+		return
+
+	for mato in matos:
+
+		var roll = randi() % 100
+
+		if roll < mato.chance_encontro:
+
+			get_tree().change_scene_to_file(
+				"res://scenes/battle.tscn"
+			)
+
+			return
 
 func _process(delta: float) -> void:
 	var direction = Input.get_vector("esquerda", "direita", "cima", "baixo")
@@ -33,3 +60,5 @@ func _process(delta: float) -> void:
 		$animaco.play(situationAtual)
 	
 	move_and_slide()
+	if direction != Vector2.ZERO:
+		tentar_encontro()
